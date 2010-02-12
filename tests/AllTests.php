50abbb93d3424da853647db3805e981a79609287
Daniel O'Connor <clockwerx@php.net>
2010-02-12 00:09:37 +0000

    Fixed AllTests.php
    git-svn-id: https://svn.php.net/repository/pear/packages/HTML_BBCodeParser/trunk@294939 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -3,7 +3,6 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
     define('PHPUnit_MAIN_METHOD', 'HTML_BBCodeParser_AllTests::main');
 }
 
-require_once 'PHPUnit/Framework/TestSuite.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
 
